71db63deff40f4d4ee0ba90808d3cd2fcdb331f2
siva270491 <49150740+siva270491@users.noreply.github.com>
2025-11-08 23:53:50 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+	
+	public static void main(String[] args) {
+		float version = 91.0f;
+		System.out.println("The version is" + " " +version);
+		String developer = "Google";
+		System.out.println("The developer is" + " " +developer);
+		boolean isBeta = false;
+		System.out.println(isBeta);
+		int releaseYear = 2008;
+		System.out.println("The release year is" + " " +releaseYear);
+		char shortcutKey = 'C';
+		System.out.println("Shortcut key is" + " " +shortcutKey);
+  }
+ }
